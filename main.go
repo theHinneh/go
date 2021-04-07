8701ffb535bda227b2255a7e3e34ec1c73a35a78
thehinneh <hinneh04@gmail.com>
2021-04-07 21:38:49 +0000

    standard lib

--- main.go
+++ main.go
@@ -13,13 +13,9 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
 	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
 	sm.Handle("/products", ph)
 
 	s := &http.Server{
