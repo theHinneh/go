ba5ac3b32f47b4b44a8396d2913f021157644438
thehinneh <hinneh04@gmail.com>
2021-04-07 22:36:32 +0000

    get, post

--- handlers/products.go
+++ handlers/products.go
@@ -21,6 +21,11 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -31,4 +36,13 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	product := &data.Product{}
+	err := product.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to m arshal json", http.StatusBadRequest)
+	}
+	data.AddProduct(product)
 }
